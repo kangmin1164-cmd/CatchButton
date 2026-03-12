78a09973ae0211d4bac65eafd47d55548783b7ec
kangmin1164 <kangmin1164@gmail.com>
2026-03-12 10:47:37 +0900

    CatchButton 생

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 255);
+            button1.Font = new Font("한컴 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = Color.Navy;
+            button1.Location = new Point(341, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 68);
+            button1.TabIndex = 0;
+            button1.Text = "나를 잡아봐";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
